dd01b457cb1f4729ccfe9b1f2eb5f21ee8fde165
Gabriel Valdivia <valdivia.gabrielvictor@gmail.com>
2026-03-29 03:52:44 -0300

    feat: adiciona funcionalidade de iniciar cronometro

--- src/app/components/Stopwatch.tsx
+++ src/app/components/Stopwatch.tsx
@@ -1,21 +1,58 @@
 'use client'
 
-import React from "react";
+import React, {RefObject, useEffect, useRef, useState} from "react";
 import {Play, RotateCcw} from 'lucide-react';
 
+function timeFormatter(totalSeconds: number) {
+    const hours: number = Math.floor(totalSeconds / 3600);
+    const minutes: number = Math.floor(totalSeconds / 60);
+    const seconds: number = totalSeconds % 60;
+
+    return [
+        String(hours).padStart(2, "0"),
+        String(minutes).padStart(2, "0"),
+        String(seconds).padStart(2, "0"),
+    ].join(":");
+}
+
 function Stopwatch() {
+
+    let [totalSeconds, setTotalSeconds] = useState(0)
+    const timerRef: RefObject<number | null> = useRef<number>(null)
+    const buttonReference: RefObject<HTMLButtonElement | null> = useRef<HTMLButtonElement>(null)
+
+    function startWatch(): void {
+        if (timerRef.current == null) {
+            timerRef.current = window.setInterval(
+                () => {
+                    // console.log(timeFormatter(totalSeconds))
+                    setTotalSeconds(totalSeconds++)
+
+                }, 1000)
+
+        }
+        return;
+    }
+
+    // "Gancho" para o react saber quando o componente foi renderizado na tela
+    useEffect(() => {
+        buttonReference.current?.focus()
+    }, []);
+
+
     return (
         <div className="flex flex-col items-center gap-8">
 
             {/* Display do Tempo - Usando fonte mono para evitar Layout Shift */}
             <h1 className="text-8xl font-light tracking-tighter sm:text-9xl font-mono">
-                00:00:00
+                {timeFormatter(totalSeconds)}
             </h1>
 
             {/* Container de Botões */}
             <div className="flex gap-4">
                 {/* Botão Iniciar - Flexbox para alinhar ícone + texto */}
                 <button
+                    onClick={startWatch}
                     className="flex items-center gap-2 min-w-[140px] justify-center rounded-full border border-white/20 bg-white px-6 py-3 text-sm font-semibold text-black transition-all hover:bg-neutral-200 active:scale-95"
                 >
                     <Play size={18} strokeWidth={2} fill="currentColor"/>
